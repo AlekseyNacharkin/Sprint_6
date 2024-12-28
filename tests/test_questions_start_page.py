392d533547ea058ec2e303da922cdaff5cae7edc
Alex <anacarkin6@gmail.com>
2024-12-28 10:43:57 +0300

    Modifying a Parameterized Test

--- tests/test_questions_start_page.py
+++ tests/test_questions_start_page.py
@@ -10,69 +10,57 @@ from selenium import webdriver
 from constants import *
 from pages.start_page import StartPage
 
+TEST_QUESTIONS_DATA = [
+    {
+        "click": StartPage.click_on_button_how_much_does_it_cost_and_how_to_pay,
+        "get_text": StartPage.get_text_price_arend,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_ABOUT_PAY_AND_COST,
+    },
+    {
+        "click": StartPage.click_on_button_arend_several_scooters,
+        "get_text": StartPage.get_text_arend_several_scooters,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTIONS_AREND_SEVERAL_SCOOTERS,
+    },
+    {
+        "click": StartPage.click_on_button_calculate_arend_time,
+        "get_text": StartPage.get_text_button_calculate_arend_time,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_CALCULATE_AREND_TIME,
+    },
+    {
+        "click": StartPage.click_on_button_order_a_scooter_today,
+        "get_text": StartPage.get_text_order_a_scooter_today,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_ORDER_A_SCOOTER_TODAY,
+    },
+    {
+        "click": StartPage.click_on_button_possible_to_extend_the_order_or_return_the_scooter_earlier,
+        "get_text": StartPage.get_text_button_possible_to_extend_the_order_or_return_the_scooter_earlier,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_POSSIBLE_TO_EXTEND_THE_ORDER
+    },
+    {
+        "click": StartPage.click_on_button_bring_the_charger_with_scooter,
+        "get_text": StartPage.get_text_button_bring_the_charger_with_scooter,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_BRING_THE_CHARGER_WITH_SCOOTER
+    },
+    {
+        "click": StartPage.click_on_button_cancel_the_order,
+        "get_text": StartPage.get_text_button_cancel_the_order,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_CANCEL_THE_ORDER
+    },
+    {
+        "click": StartPage.click_on_button_info_areal_scooter_arend,
+        "get_text": StartPage.get_text_button_info_areal_scooter_arend,
+        "expected_text": TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTONS_INFO_AREAL_SCOOTER_AREND
+    }]
+
 
 class TestQuestion():
 
-    @allure.title("Тест текста вопроса 'Сколько это стоит? И как оплатить?'")
-    def test_question_about_pay_and_cost(self,driver):
+    @allure.title("Тест текстов вопроса")
+    @pytest.mark.parametrize("data", TEST_QUESTIONS_DATA)
+    def test_questions(self, driver, data):
         start_page = StartPage(driver)
         start_page.get_base_url()
-        start_page.click_on_button_how_much_does_it_cost_and_how_to_pay()
-        text = start_page.get_text_price_arend()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_ABOUT_PAY_AND_COST
+        data["click"](start_page)
+        actual_text = data["get_text"](start_page)
+        assert actual_text == data["expected_text"]
 
-    @allure.title("Тест текста вопроса 'Хочу сразу несколько самокатов! Так можно?'")
-    def test_question_arend_several_scooters(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_arend_several_scooters()
-        text = start_page.get_text_arend_several_scooters()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTIONS_AREND_SEVERAL_SCOOTERS
-
-    @allure.title("Тест текста вопроса 'Как рассчитывается время аренды?'")
-    def test_question_button_calculate_arend_time(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_calculate_arend_time()
-        text = start_page.get_text_button_calculate_arend_time()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_CALCULATE_AREND_TIME
-
-    @allure.title("Тест текста вопроса 'Можно ли заказать самокат прямо на сегодня?'")
-    def test_question_order_a_scooter_today(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_order_a_scooter_today()
-        text = start_page.get_text_order_a_scooter_today()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_ORDER_A_SCOOTER_TODAY
-
-    @allure.title("Тест текста вопроса 'Можно ли продлить заказ или вернуть самокат раньше?'")
-    def test_question_possible_to_extend_the_order_or_return_the_scooter_earlier(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_possible_to_extend_the_order_or_return_the_scooter_earlier()
-        text = start_page.get_text_button_possible_to_extend_the_order_or_return_the_scooter_earlier()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_POSSIBLE_TO_EXTEND_THE_ORDER
-
-    @allure.title("Тест текста вопроса 'Вы привозите зарядку вместе с самокатом?'")
-    def test_question_button_bring_the_charger_with_scooter(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_bring_the_charger_with_scooter()
-        text = start_page.get_text_button_bring_the_charger_with_scooter()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_BRING_THE_CHARGER_WITH_SCOOTER
-
-    @allure.title("Тест текста вопроса 'Можно ли отменить заказ?'")
-    def test_question_button_cancel_the_order(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_cancel_the_order()
-        text = start_page.get_text_button_cancel_the_order()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTON_CANCEL_THE_ORDER
-
-    @allure.title("Тест текста вопроса 'Я жизу за МКАДом, привезёте?'")
-    def test_question_button_info_areal_scooter_arend(self,driver):
-        start_page = StartPage(driver)
-        start_page.get_base_url()
-        start_page.click_on_button_info_areal_scooter_arend()
-        text = start_page.get_text_button_info_areal_scooter_arend()
-        assert text == TestQuestions.ASSERTION_TEXT_TEST_QUESTION_BUTTONS_INFO_AREAL_SCOOTER_AREND
